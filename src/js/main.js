d28281a068af49b57e13d654c23f923e8a9468c3
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2016-01-27 14:33:25 +0100

    first commit - MartijnN

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// var MapHandler = require('./modules/map/mapHandler');
+
+// var main = new Main();
+// main.init();
+
+// function Main() {
+
+// 	var _this = this;
+// 	var _mapHandler;
+// 	var _infboxHandler;
+
+// 	_this.init = function() {
+
+// 	 	_mapHandler = new MapHandler();
+
+// 	}
+
+// }
